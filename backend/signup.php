9275ac4eac44190b4044dd104a54251ce78d9bc4
KarimToumi123 <karimtoumi396@gmail.com>
2025-06-02 17:21:10 +0100

    first

--- backend/signup.php
+++ backend/signup.php
@@ -3,23 +3,12 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-<<<<<<< HEAD
-
-=======
 require_once('entities\user.class.php');
 require_once('config.php');
->>>>>>> 227fdf6 (addd login)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
-<<<<<<< HEAD
-
-require_once('user.class.php');
-require_once('config.php');
-
-=======
->>>>>>> 227fdf6 (addd login)
 $us = new user();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -37,22 +26,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $us->last_name = $data["last_name"] ?? "";
     $us->phone = $data["phone"] ?? "";
     $us->role = $data["role"] ?? "user";
-<<<<<<< HEAD
-=======
     //hachage du mdp
     $us->pwd = password_hash($us->pwd, PASSWORD_DEFAULT);
 
->>>>>>> 227fdf6 (addd login)
 
     if ($us->recherche_user() == null) {
         $cnx = new connexion();
         $pdo = $cnx->CNXbase();
-<<<<<<< HEAD
-        $req = "INSERT INTO user (username, last_name, phone, email, password, role) VALUES
-                ('$us->username', '$us->last_name', '$us->phone', '$us->email', '$us->pwd', '$us->role')";
-        try {
-            $pdo->exec($req);
-=======
         
         
         try {
@@ -66,7 +46,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 ':password' => $us->pwd,
                 ':role' => $us->role
             ]);
->>>>>>> 227fdf6 (addd login)
             echo json_encode(["status" => "success", "message" => "User registered"]);
         } catch (PDOException $e) {
             http_response_code(500);
@@ -80,3 +59,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     http_response_code(405);
     echo json_encode(["status" => "error", "message" => "Method not allowed"]);
 }
+?>
